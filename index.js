eeafcb50e3eeaf6c38e0f48e8368e7b1b12cff1b
y60509796-pixel <y60509796@gmail.com>
2026-03-05 05:46:47 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.on("ready", () => {
+  console.log(`ログイン成功: ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+
+  if (message.content === "!ping") {
+    message.reply("pong!");
+  }
+
+});
+
+client.login(process.env.TOKEN);
